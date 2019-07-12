eff35747f6236e32ac6a15af51cc8629f7317b65
YeisonGomez <yeigomez97@hotmail.com>
2019-07-11 20:16:07 -0500

    refactor(movie): roles in method create

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Request } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateDto } from './dto/create.dto'
 import { AuthGuard } from '@nestjs/passport';
@@ -10,19 +10,22 @@ export class MovieController {
     constructor(private readonly movieService: MovieService) {}
     
     @Get('all')
-    @UseGuards(AuthGuard('bearer'), RolesGuard)
-    @Roles('client')
-    getAll() {
+    @UseGuards(AuthGuard('bearer'))
+    getAll() { 
         return this.movieService.findAll();
     }
 
     @Get('get/:id/:id2')
-    getById(@Param('id') id, @Param('id2') id2) {
+    @UseGuards(AuthGuard('bearer'))
+    getById(@Param('id') id, @Param('id2') id2, @Request() req) {
+        console.log(req.user);
         console.log(id);
         return this.movieService.getById(id);
     }
 
-    @Post()
+    @Post('create')
+    @UseGuards(AuthGuard('bearer'), RolesGuard)
+    @Roles('admin')
     create(@Body() body: CreateDto) {
         console.log(body);
         return this.movieService.create(body);
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -7,16 +7,20 @@ import { createDiffieHellman } from 'crypto';
 @Injectable()
 export class MovieService {
     constructor(
-        @InjectRepository(movie)
-        private readonly movieRepository: Repository<movie>
+        @InjectRepository(movie) private readonly movieRepository: Repository<movie>
       ) {}
 
+      //lowerCamelCase = Variables, metodos
+      //UpperCamelCase = Nombres de clases
+      //snake_camel_case = Base de datos
       findAll() {
         return this.movieRepository.find({ relations: ["fkGenre"] });
       }
 
       getById(id) {
         return this.movieRepository.createQueryBuilder()
+        .select("id", "movieId")
+        .addSelect("name", "movieNames")
         .where("id = :ids", { ids: 1 })
         .execute();
       }
